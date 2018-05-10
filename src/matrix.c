95620a6aefd5a4c70850ec07fb4f7cf33a1fbd7a
Mourey jerome <jerome@everycheck.fr>
2018-05-10 10:55:56 +0200

    add funciton

--- src/matrix.c
+++ src/matrix.c
@@ -43,6 +43,41 @@ unsigned int  matrix_is_valid(Matrix* matrix)
 	return 1;
 }
 
+Matrix* matrix_copy(Matrix* matrix)
+{
+	Matrix* out = matrix_new(matrix->rows,matrix->columns,0.0);
+	const unsigned int size = matrix->rows*matrix->columns;
+	for (unsigned int i = 0; i < size; i++)
+	{
+		out->data[i] = matrix->data[i];
+	}
+	return out;
+}
+
+Matrix* matrix_transpose(Matrix* matrix)
+{
+	Matrix* out = matrix_new(matrix->columns,matrix->rows,0.0);
+	for (unsigned int i = 0; i < matrix->rows; i++)
+	{	
+		for (unsigned int j = 0; j < matrix->columns; j++)
+		{
+			out->data[i+j*matrix->rows] = matrix->data[j+i*matrix->columns];
+		}
+	}
+	return out;
+}
+
+
+double matrix_get(Matrix* matrix, unsigned int row, unsigned int column)
+{
+	return matrix->data[column+row*matrix->columns];
+}
+
+void   matrix_set(Matrix* matrix, unsigned int row, unsigned int column,double value)
+{
+	matrix->data[column+row*matrix->columns] = value;
+}
+
 void matrix_print(Matrix* matrix)
 {
 	if (matrix_is_valid(matrix) == 0) return;
@@ -52,15 +87,26 @@ void matrix_print(Matrix* matrix)
 	{	
 		for (unsigned int j = 0; j < matrix->columns; j++)
 		{
-			printf("%2.3lf,",matrix->data[i+j*matrix->columns]);
+			printf("%2.3lf,",matrix->data[j+i*matrix->columns]);
 		}
 		printf("\n");
 	}
 	printf("\n");
 
-
 }
 
+Matrix* matrix_add(Matrix* m1, Matrix* m2)
+{
+	if(m1->columns != m2->columns || m1->rows != m2->rows) return 0;
+
+	Matrix* out = matrix_new(m1->rows,m1->columns,0.0);
+	const unsigned int size = m1->rows*m1->columns;
+	for (unsigned int i = 0; i < size; i++)
+	{
+		out->data[i] = m1->data[i] + m2->data[i];
+	}
+	return out;
+}
 
 
 unsigned int matrix_get_ref_counter()
--- test/main.c
+++ test/main.c
@@ -5,10 +5,32 @@
 
 int main()
 {
-	Matrix* m = matrix_new(3,3,0.0);
-	printf("hello world!\n");
+	Matrix* m = matrix_new(2,4,1.0);
+	for (unsigned int i = 0; i < 8; ++i)
+	{
+		m->data[i] = (double) i;
+	}
+	matrix_set(m,0,3,1.0);
+	printf("%2.3lf\n",matrix_get(m,1,3) );
+
+	Matrix* m2 = matrix_transpose(m);
 	matrix_print(m);
+	matrix_print(m2);
+
 	matrix_delete(m);
+	matrix_delete(m2);
+
+
+	m = matrix_new(2,4,1.0);
+	for (unsigned int i = 0; i < 8; ++i)
+	{
+		m->data[i] = (double) i;
+	}
+	m2 = matrix_add(m,m);
+	matrix_print(m);
+	matrix_print(m2);
+	matrix_delete(m);
+	matrix_delete(m2);
 
 	printf("Matrix forget : %u!\n",matrix_get_ref_counter());
 	return 0;
