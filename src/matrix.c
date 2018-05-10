ab8018c8f66d405aef77e091df390e9c00bca0b5
Mourey jerome <jerome@everycheck.fr>
2018-05-10 11:23:55 +0200

    implement everything

--- src/matrix.c
+++ src/matrix.c
@@ -108,6 +108,94 @@ Matrix* matrix_add(Matrix* m1, Matrix* m2)
 	return out;
 }
 
+Matrix* matrix_addnum(Matrix* matrix,double value)
+{
+	Matrix* out = matrix_new(matrix->rows,matrix->columns,0.0);
+	const unsigned int size = matrix->rows*matrix->columns;
+	for (unsigned int i = 0; i < size; i++)
+	{
+		out->data[i] = matrix->data[i] + value;
+	}
+	return out;
+}
+
+Matrix* matrix_sub(Matrix* m1, Matrix* m2)
+{
+	if(m1->columns != m2->columns || m1->rows != m2->rows) return 0;
+
+	Matrix* out = matrix_new(m1->rows,m1->columns,0.0);
+	const unsigned int size = m1->rows*m1->columns;
+	for (unsigned int i = 0; i < size; i++)
+	{
+		out->data[i] = m1->data[i] - m2->data[i];
+	}
+	return out;
+}
+
+Matrix* matrix_mul(Matrix* m1, Matrix* m2)
+{
+	if(m1->columns != m2->rows || m1->rows != m2->columns) return 0;
+
+	Matrix* out = matrix_new(m1->rows,m2->columns,0.0);
+
+	for (unsigned int i = 0; i < m1->rows; i++)
+	{	
+		for (unsigned int j = 0; j < m2->columns; j++)
+		{
+			double result = 0.0;
+			for (unsigned int k = 0; k < m2->rows; k++)
+			{
+				//num = num + m1.mtx[i][n] * m2.mtx[n][j]
+				printf("i %u, j %u, k %u, m1 %2.3lf, m2 %2.3lf\n",i,j,k, m1->data[k+i*m1->columns] , m2->data[j+k*m2->columns]);
+				result += m1->data[k+i*m1->columns] * m2->data[j+k*m2->columns];
+			}
+			out->data[i+j*out->columns] = result;	
+			printf("i %u, j %u,  out %2.3lf\n",i,j,result);
+
+		}
+	}
+	return out;
+}
+
+
+Matrix*       matrix_mulnum(Matrix* m1, Matrix* m2)
+{
+	Matrix* out = matrix_new(matrix->rows,matrix->columns,0.0);
+	const unsigned int size = matrix->rows*matrix->columns;
+	for (unsigned int i = 0; i < size; i++)
+	{
+		out->data[i] = matrix->data[i] * value;
+	}
+	return out;
+}
+
+Matrix*  matrix_hadamard_mul(Matrix* matrix,double value)
+{
+	if(m1->columns != m2->columns || m1->rows != m2->rows) return 0;
+
+	Matrix* out = matrix_new(m1->rows,m1->columns,0.0);
+	const unsigned int size = m1->rows*m1->columns;
+	for (unsigned int i = 0; i < size; i++)
+	{
+		out->data[i] = m1->data[i] * m2->data[i];
+	}
+	return out;
+}
+
+unsigned char matrix_is_equal(Matrix* m1, Matrix* m2)
+{
+	if (m1 == m2 ) return 1;
+	if(m1->columns != m2->columns || m1->rows != m2->rows) return 0;
+	if( m1.data = m2.data2) return 1
+	const double epsilon = 0.000000000001;
+
+	const unsigned int size = m1->rows*m1->columns;
+	for (unsigned int i = 0; i < size; i++)
+	{
+		if( fabs(m1->data[i] - m2->data[i]) > epsilon ) return 0;
+	}
+	return 1;
+}
 
 unsigned int matrix_get_ref_counter()
 {
--- test/main.c
+++ test/main.c
@@ -32,6 +32,28 @@ int main()
 	matrix_delete(m);
 	matrix_delete(m2);
 
+
+	// 1,2,3   * 1,4   =   14,32
+	// 4,5,6     2,5       32,77
+	//           3,6
+
+	m = matrix_new(2,3,1.0);
+	for (unsigned int i = 0; i < 6; ++i)
+	{
+		m->data[i] = (double) i+1;
+	}	
+
+	m2 = matrix_transpose(m);
+	matrix_print(m);
+	matrix_print(m2);
+
+	Matrix* m3 =  matrix_mul(m,m2);
+	matrix_print(m3);
+
+	matrix_delete(m);
+	matrix_delete(m2);
+	matrix_delete(m3);
+
 	printf("Matrix forget : %u!\n",matrix_get_ref_counter());
 	return 0;
 }
