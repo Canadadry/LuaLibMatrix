f0273dd7acd2035f87a8b2b35c1380063ef2de73
Canadadry <jerome@everycheck.fr>
2018-05-15 17:09:04 +0200

    bind all function

--- src/lua_matrix.c
+++ src/lua_matrix.c
@@ -5,7 +5,12 @@
 #include "lauxlib.h"
 #include "matrix.h"
 
-int matrinx_constructor(lua_State * l)
+Matrix* check_type(lua_State * l, int n)
+{
+    return *(Matrix **)luaL_checkudata(l, n, "MatrixeMetatableName");
+}
+
+int lua_matrix_new(lua_State * l)
 {
     printf("matrix_constructor\n");
 
@@ -20,12 +25,8 @@ int matrinx_constructor(lua_State * l)
     return 1;
 }
 
-Matrix* check_type(lua_State * l, int n)
-{
-    return *(Matrix **)luaL_checkudata(l, n, "MatrixeMetatableName");
-}
 
-int destructor(lua_State * l)
+int lua_matrix_free(lua_State * l)
 {
     printf("destructor\n");
     Matrix* matrix = check_type(l, 1);
@@ -33,7 +34,7 @@ int destructor(lua_State * l)
     return 0;
 }
 
-int print_type(lua_State * l)
+int lua_matrix_print(lua_State * l)
 {
     Matrix* ma = check_type(l, 1);
 	const unsigned int maxSizeCell = 20;
@@ -46,10 +47,155 @@ int print_type(lua_State * l)
     return 1;
 }
 
+int lua_matrix_copy(lua_State * l)
+{
+    Matrix* ma = check_type(l, 1);
+
+    Matrix ** udata = (Matrix **)lua_newuserdata(l, sizeof(Matrix *));
+    *udata = matrix_copy(ma);
+    luaL_getmetatable(l, "MatrixeMetatableName" );
+    lua_setmetatable(l, -2);
+    return 1;
+}
+
+int lua_matrix_transpose(lua_State * l)
+{
+    Matrix* ma = check_type(l, 1);
+
+    Matrix ** udata = (Matrix **)lua_newuserdata(l, sizeof(Matrix *));
+    *udata = matrix_transpose(ma);
+    luaL_getmetatable(l, "MatrixeMetatableName" );
+    lua_setmetatable(l, -2);
+    return 1;
+}
+
+int lua_matrix_get(lua_State * l)
+{
+    Matrix* ma = check_type(l, 1);
+    const unsigned int row    = luaL_checknumber (l, 2);
+    const unsigned int column    = luaL_checknumber (l, 3);
+
+    const double data = matrix_get(ma,row,column);
+    lua_pushnumber(l, data);
+    return 1;
+}
+
+int lua_matrix_set(lua_State * l)
+{
+    Matrix* ma = check_type(l, 1);
+    const unsigned int row    = luaL_checknumber (l, 2);
+    const unsigned int column    = luaL_checknumber (l, 3);
+    const double value    = luaL_checknumber (l, 4);
+    
+    matrix_set(ma,row,column,value);
+    
+    return 0;
+}
+
+int lua_matrix_add(lua_State * l)
+{
+    Matrix* m1 = check_type(l, 1);
+    Matrix* m2 = check_type(l, 2);
+
+    Matrix ** udata = (Matrix **)lua_newuserdata(l, sizeof(Matrix *));
+    *udata = matrix_add(m1,m2);
+    luaL_getmetatable(l, "MatrixeMetatableName" );
+    lua_setmetatable(l, -2);
+    return 1;
+}
+
+int lua_matrix_addnum(lua_State * l)
+{
+    Matrix* m1   = check_type(l, 1);
+    double value = luaL_checknumber (l, 2);
+
+    Matrix ** udata = (Matrix **)lua_newuserdata(l, sizeof(Matrix *));
+    *udata = matrix_addnum(m1,value);
+    luaL_getmetatable(l, "MatrixeMetatableName" );
+    lua_setmetatable(l, -2);
+    return 1;
+}
+
+int lua_matrix_sub(lua_State * l)
+{
+    Matrix* m1 = check_type(l, 1);
+    Matrix* m2 = check_type(l, 2);
+
+    Matrix ** udata = (Matrix **)lua_newuserdata(l, sizeof(Matrix *));
+    *udata = matrix_sub(m1,m2);
+    luaL_getmetatable(l, "MatrixeMetatableName" );
+    lua_setmetatable(l, -2);
+    return 1;
+}
+
+int lua_matrix_mul(lua_State * l)
+{
+    Matrix* m1 = check_type(l, 1);
+    Matrix* m2 = check_type(l, 2);
+
+    Matrix ** udata = (Matrix **)lua_newuserdata(l, sizeof(Matrix *));
+    *udata = matrix_mul(m1,m2);
+    luaL_getmetatable(l, "MatrixeMetatableName" );
+    lua_setmetatable(l, -2);
+    return 1;
+}
+
+int lua_matrix_mulnum(lua_State * l)
+{
+    Matrix* m1   = check_type(l, 1);
+    double value = luaL_checknumber (l, 2);
+
+    Matrix ** udata = (Matrix **)lua_newuserdata(l, sizeof(Matrix *));
+    *udata = matrix_mulnum(m1,value);
+    luaL_getmetatable(l, "MatrixeMetatableName" );
+    lua_setmetatable(l, -2);
+    return 1;
+}
+
+int lua_matrix_hadamard_mul(lua_State * l)
+{
+    Matrix* m1 = check_type(l, 1);
+    Matrix* m2 = check_type(l, 2);
+
+    Matrix ** udata = (Matrix **)lua_newuserdata(l, sizeof(Matrix *));
+    *udata = matrix_hadamard_mul(m1,m2);
+    luaL_getmetatable(l, "MatrixeMetatableName" );
+    lua_setmetatable(l, -2);
+    return 1;
+}
+
+int lua_matrix_is_equal(lua_State * l)
+{
+    Matrix* m1 = check_type(l, 1);
+    Matrix* m2 = check_type(l, 2);
+
+    unsigned char out = matrix_is_equal(m1,m2);
+    lua_pushboolean(l, out);
+    return 1;
+}
+
+int lua_matrix_get_ref_counter(lua_State * l)
+{
+    lua_pushnumber(l, matrix_get_ref_counter());
+    return 1;
+}
+
 static const struct luaL_Reg functions [] = {
-    { "new"         , matrinx_constructor },
-    { "__gc"        , destructor      },
-    { "__tostring"  , print_type  },
+    { "new"         , lua_matrix_new },
+    { "__gc"        , lua_matrix_free      },
+    { "__tostring"  , lua_matrix_print  },
+    { "copy"  , lua_matrix_copy  },
+    { "transpose"  , lua_matrix_transpose  },
+    { "get"  , lua_matrix_get  },
+    { "set"  , lua_matrix_set  },
+    { "add"  , lua_matrix_add  },
+    { "addnum"  , lua_matrix_addnum  },
+    { "sub"  , lua_matrix_sub  },
+    { "mul"  , lua_matrix_mul  },
+    { "mulnum"  , lua_matrix_mulnum  },
+    { "hadamard_mul"  , lua_matrix_hadamard_mul  },
+    { "is_equal"  , lua_matrix_is_equal  },
+    { "count", lua_matrix_get_ref_counter},
     {NULL, NULL}
 };
 
--- test/main.lua
+++ test/main.lua
@@ -5,6 +5,65 @@ hello = require "lua_matrix"
 print(Matrix)
 print(type(Matrix))
 
-local value = Matrix.new(3,3,1)
-print(value)
-print(type(value))
+local m = Matrix.new(2,4,1)
+
+local count = 0
+for i=0,1 do
+	for j=0,3 do
+		m:set(i,j,count)
+		count= count + 1
+	end
+end
+m:set(0,3,1)
+print(m:get(1,3))
+
+local m2 = m:transpose()
+print(m)
+print(m2)
+
+m = Matrix.new(2,4,1)
+
+local count = 0
+for i=0,1 do
+	for j=0,3 do
+		m:set(i,j,count)
+		count= count + 1
+	end
+end
+
+m2 = m:add(m)
+
+
+print(m)
+print(m2)
+
+	-- 1,2,3   * 1,4   =   14,32
+	-- 4,5,6     2,5       32,77
+	--           3,6
+
+
+m = Matrix.new(2,3,1)
+
+local count = 1
+for i=0,1 do
+	for j=0,2 do
+		m:set(i,j,count)
+		count= count + 1
+	end
+end
+
+m2 = m:transpose();
+
+
+print(m)
+print(m2)
+
+print(m:mul(m2))
+
+print("Matrix forget : ".. Matrix.count());
+collectgarbage()
+print("Matrix forget : ".. Matrix.count());
+m = nil
+m2 = nil
+collectgarbage()
+print("Matrix forget : ".. Matrix.count());
